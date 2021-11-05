d93736b0b91d0602f00c64ed717a337e19ccecea
mplza <46725126+mplza@users.noreply.github.com>
2021-11-06 00:02:51 +0200

    Add files via upload

---
+++ R koolitus ülesanne juhised.R
@@ -0,0 +1,30 @@
+# Lae töölauale paketid: tidyverse, lubridate
+
+
+# Lae töölauale andmestik Superstore.xlsx
+
+
+# Nimeta andmestik ümber: df
+
+
+# Loo ülevaade andmestikust kasutades funktsioone head(), glimpse() ja summary()
+
+
+# 1. küsimus
+# Millised on poe erinevad kliendisegmendid, tootekategooriad ja alamkategooriad?
+
+
+# 2. küsimus
+# Mitu erinevat toodet kuulub igasse tootekategooriasse ja alamkategooriasse?
+# Reasta tulemused kategooriate kaupa suuremast väiksemani.
+
+
+# 3. küsimus
+# Mis on iga kliendisegmendi kõige enimmüüdud toode?
+
+
+# 4. küsimus
+# Mitu tellimust tegid Wisconsini osariigi ärikliendid 2016. aasta septembris?
+
+
+# 5. Koosta joonis, mis kujutab telefonide müügitulemusi kliendisegmentide kaupa 2017. aastal.
